d60ab657a01cdff92d0090ea555894220dd1da4b
coelhovinicius <coelhovinicius@yahoo.com.br>
2022-01-04 21:04:50 -0300

    Generics

---
+++ Aula204_Generics/PrintService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Aula204_Generics
+{
+    class PrintService
+    {
+        private int[] _values = new int[10]; // Vetor privado para armazenar ate 10 numeros inteiros
+        private int _count = 0; // Variavel privada para fazer a contagem dos numeros inseridos
+
+        // Operacoes
+        public void AddValue(int value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("PrintService is full");
+            }
+            _values[_count] = value; // Adiciona o valor informado como argumento dentro do vetor na posicao "_count"
+            _count++;
+        }
+
+        public int First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("PrintService is empty");
+            }
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count -1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+            if (_count > 0)
+            {
+                Console.Write(_values[_count -1]);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
